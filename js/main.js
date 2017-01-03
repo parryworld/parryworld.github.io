70f3a20515421a26dd8e7a8aadea9dd18d4ff0ac
parryworld <parryworld@gmail.com>
2017-01-03 14:59:04 +0800

    portfolio 1.0.0

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+var windowHeight = $(window).height();
+
 $(document).ready(function() {
-  $('#fullpage').fullpage();
+  $(window).scroll(function(e) {
+    var bottomOfWindow = $(window).scrollTop() + $(window).height();
+    var topOfElement = $(".content-container").offset().top;
+    var difference = bottomOfWindow - topOfElement;
+    var max = windowHeight / 2;
+    var opacity = difference / max;
+    $(".cover").css("opacity", 1 - opacity);
+    $(".portfolio").css("opacity", opacity);
+  });
 });

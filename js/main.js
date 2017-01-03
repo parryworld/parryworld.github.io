dd2953947bb6ba5ef98a7e0dc97f4152b6952811
parryworld <parryworld@gmail.com>
2017-01-03 19:06:41 +0800

    移动端适配

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-var windowHeight = $(window).height();
-
 $(document).ready(function() {
+  var windowHeight = $(window).height();
+  
+  $("body").scrollTop(0);
+
   $(window).scroll(function(e) {
     var bottomOfWindow = $(window).scrollTop() + $(window).height();
     var topOfElement = $(".content-container").offset().top;
